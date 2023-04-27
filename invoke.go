199127b0fe222f893c24f84173317432cea80f4c
S3846835@student.rmit.edu.au <S3846835@student.rmit.edu.au>
2023-04-27 00:56:06 +0000

    dbt connect gcp test 2

--- invoke.go
+++ invoke.go
@@ -1,4 +1,5 @@
 package main
+
 import (
         "fmt"
         "log"
@@ -6,23 +7,29 @@ import (
         "os"
         "os/exec"
 )
+
 func handler(w http.ResponseWriter, r *http.Request) {
         log.Print("helloworld: received a request")
-cmd := exec.Command("/bin/sh", "script.sh")
-        cmd.Stdout = os.Stdout
- cmd.Stderr = os.Stderr
- err := cmd.Run()
- if err != nil {
-  log.Fatalf("cmd.Run() failed with %s\n", err)
- }
+
+        cmd := exec.CommandContext(r.Context(), "/bin/sh", "script.sh")
+        cmd.Stderr = os.Stderr
+        out, err := cmd.Output()
+        if err != nil {
+                w.WriteHeader(500)
+        }
+        w.Write(out)
 }
+
 func main() {
         log.Print("helloworld: starting server...")
-http.HandleFunc("/", handler)
-port := os.Getenv("PORT")
+
+        http.HandleFunc("/", handler)
+
+        port := os.Getenv("PORT")
         if port == "" {
                 port = "8080"
         }
-log.Printf("helloworld: listening on %s", port)
+
+        log.Printf("helloworld: listening on %s", port)
         log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
