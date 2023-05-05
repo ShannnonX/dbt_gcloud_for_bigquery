5b42e149946508ced0d9e9c7108c85892e3cff59
S3846835@student.rmit.edu.au <S3846835@student.rmit.edu.au>
2023-05-05 06:18:41 +0000

    dbt connect gcp test 5

--- models/silver_less_column_version_update_test_showcase.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        materialized = 'table'
-    )
-}}
-
-
-SELECT * FROM `house-prediction-381923.melbourne_house_data.bronze_less_column_version_updated_test_showcase`   WHERE Price IS NOT NULL 
