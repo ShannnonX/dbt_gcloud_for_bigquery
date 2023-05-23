5b7d487fba3bea437f90cfadb7398fd9b151050c
S3846835@student.rmit.edu.au <S3846835@student.rmit.edu.au>
2023-05-23 12:09:27 +0000

    dbt connect gcp test 8-4 (delete outliers, and merge regionname)

--- models/silver_full_column_version_updated_total.sql
+++ models/silver_full_column_version_updated_total.sql
@@ -21,11 +21,18 @@ date_ranges AS (
 )
 
 SELECT
-  * EXCEPT(latest_date, test_start_date, validate_start_date),
+  * EXCEPT(latest_date, test_start_date, validate_start_date, Regionname),
   CASE
     WHEN date >= test_start_date THEN 'Test'
     WHEN (date >= validate_start_date and date < test_start_date) THEN 'Validate'
     WHEN date < validate_start_date THEN 'Train'
-  END AS dataType
+  END AS dataType, 
+  CASE
+      WHEN Regionname = 'Eastern Victoria' THEN 'Eastern Metropolitan'
+      WHEN Regionname = 'Western Victoria' THEN 'Western Metropolitan'
+      WHEN Regionname = 'Northern Victoria' THEN 'Northern Metropolitan'
+      ELSE Regionname
+  END AS Regionname
 FROM
   all_data, date_ranges
+WHERE landsize < 2000 and price < 9000000.0
